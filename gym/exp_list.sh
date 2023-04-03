c00de4c4adcdddcd39f37110d57f2d537dac23a8
jesn1219 <jesn1219@gmail.com>
2023-04-03 08:51:22 +0000

    230403

---
+++ gym/exp_list.sh
@@ -0,0 +1,6 @@
+
+
+# 230403
+
+
+python experiment.py --env hopper --dataset medium --model_type dt  --log_to_wandb True --max_iters 50
---
+++ gym/tmux_exp.sh
@@ -0,0 +1,8 @@
+# This script is used to run experiments in tmux sessions
+# In 1 session, 
+
+
+
+
+
+python experiment.py --env hopper --dataset medium --model_type dt  --log_to_wandb True --max_iters 20
